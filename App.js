78aeaff19f0204481fd67664a2c5c1ca49c76d40
EJ <ej.roux13@gmail.com>
2022-08-08 07:20:24 -0700

    testing stack navigator

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SignUp from './screens/signUp';
+import Login from './screens/login';
+import Navigator from './routes/loginStack'
 
 export default function App() {
 
   return (
-    <SignUp />
+    // <SignUp />
+    // <Login />
+    <Navigator />
   );
 }
