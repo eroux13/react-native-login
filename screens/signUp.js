37670bcbec2c79a87da7d2e059935112b86882d4
EJ <ej.roux13@gmail.com>
2022-08-07 22:06:58 -0700

    separated pages into individual components

---
+++ screens/signUp.js
@@ -0,0 +1,98 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity
+} from 'react-native';
+
+export default function SignUp() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/favicon.png')} />
+      <Text style={styles.appName}>Vindicators Image Recognition</Text>
+
+      <StatusBar style='auto' />
+      <View style={styles.inputView}>
+        <TextInput style={styles.TextInput}
+          placeholder='Email'
+          placeholderTextColor='#003f5c'
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput style={styles.TextInput}
+          placeholder='Password'
+          placeholderTextColor='#003f5c'
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput style={styles.TextInput}
+          placeholder='Confirm Password'
+          placeholderTextColor='#003f5c'
+          secureTextEntry={true}
+          onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButton}>SIGN UP</Text> 
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginBottom: 20,
+  },
+  appName: {
+    marginBottom: 40,
+  },
+  inputView: {
+    backgroundColor: '#FFC0CB',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+    alignItems: 'center'
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgotButton: {
+
+  },
+  loginButton: {
+    width: '80%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    backgroundColor: '#FF1493',
+  },
+});
