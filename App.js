2ac6e1be67faa95d75783b7b72312d6dc8a97786
EJ <ej.roux13@gmail.com>
2022-08-09 23:44:53 -0700

    removed imports

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import SignUp from './screens/signUp';
-import Login from './screens/login';
 import Navigator from './routes/loginStack'
 
 export default function App() {
 
   return (
-    // <SignUp />
-    // <Login />
     <Navigator />
   );
 }
