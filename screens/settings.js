3fc5bc0546c90f62040f432ca7b95504a5d476ba
EJ <ej.roux13@gmail.com>
2022-08-22 16:12:23 -0700

    updated export name

--- screens/settings.js
+++ screens/settings.js
@@ -4,7 +4,7 @@ import { TextInput, Button } from "react-native-paper";
 import { Text, View, Image } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function SignUp({ navigation }) {
+export default function Settings() {
 
   return (
     <View style={globalStyles.container}>
