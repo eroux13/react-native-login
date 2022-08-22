6d1a1171b29a8c47f6e7c9b39ace8107e04ae44a
EJ <ej.roux13@gmail.com>
2022-08-21 18:53:52 -0700

    removed test page

--- routes/loginStack.js
+++ routes/loginStack.js
@@ -2,12 +2,8 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import SignUp from '../screens/signUp';
 import Login from '../screens/login';
-import Test from '../screens/testPage'
 
 const screens = {
-    Test: {
-        screen: Test
-    },
     SignUp: {
         screen: SignUp
     },
