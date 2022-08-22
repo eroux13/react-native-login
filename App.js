89ce77b228b5443ae7e875f841b9758c9e9c363a
EJ <ej.roux13@gmail.com>
2022-08-22 14:47:25 -0700

    testing bottom navigation

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Navigator from './routes/loginStack';
+import MainTabs from './screens/mainContainer';
 
 export default function App() {
 
   return (
-    <Navigator />
+    <NavigationContainer>
+      <Navigator />
+      {/* <MainTabs /> */}
+    </NavigationContainer>
   );
 }
