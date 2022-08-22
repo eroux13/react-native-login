d409d5fe56610cc6f1929da0d52391502c9dd22f
EJ <ej.roux13@gmail.com>
2022-08-22 16:11:45 -0700

    added and testing camera permissions

--- screens/camera.js
+++ screens/camera.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { TextInput, Button } from "react-native-paper";
-import { Text, View, Image } from 'react-native';
+import { Button } from "react-native-paper";
+import { Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { Camera, CameraType } from 'expo-camera';
 
-export default function SignUp({ navigation }) {
+export default function Cam() {
+  
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();  
+  
+  if (!permission) {
+    return <View />
+  }
 
+  if (!permission.granted) {
+    return (
+        <View>
+            <Text> We need your permission to use camera</Text>
+            <Button mode='contained' onPress={requestPermission}>Grant Permission</Button>
+        </View>
+    )
+  }
   return (
     <View style={globalStyles.container}>
         <StatusBar style='dark' />
